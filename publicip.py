8ba717522e338478831749f6df2dbc54cc19f756
Chaithanya Allu <chait408@gmail.com>
2020-07-21 22:56:26 -0700

    Updated public ip url

--- publicip.py
+++ publicip.py
@@ -2,6 +2,6 @@ import requests
 import json
 
 def get_ip_info():
-    response = requests.get('https://ipinfo.io')
+    response = requests.get('https://ifconfig.co/json')
     public_ip_info = json.loads(response.content.decode('utf-8'))
-    return (public_ip_info['ip'], public_ip_info['region'], public_ip_info['org'])
+    return (public_ip_info['ip'], public_ip_info['region_name'], public_ip_info['asn_org'])
